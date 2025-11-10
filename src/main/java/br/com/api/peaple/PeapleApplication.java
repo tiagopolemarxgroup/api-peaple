fb273b193a600e5381fcb099bfd48a4df267f92d
Tiago <tiago.polemarxgroup@gmail.com>
2025-11-10 04:53:31 -0300

    Projeto criado

---
+++ src/main/java/br/com/api/peaple/PeapleApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.peaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeapleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeapleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/api/peaple/PeapleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.peaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeapleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
